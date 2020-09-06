2ddcf380d7cf88bbadaa16867a7f09be736ae8ef
dogrod <rodrickjue@hotmail.com>
2020-09-06 21:02:58 +0800

    :truck: Rename html title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/todomvc-vue-composition-api/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.ts',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Vue Composition API · TodoMVC',
+    },
+  }
 }
